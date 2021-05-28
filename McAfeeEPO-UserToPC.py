677737e9cb3ad333d303497660cd56130dfc348a
RichardAG <39999483+Richard1611@users.noreply.github.com>
2021-05-28 11:34:24 -0400

    Add files via upload

---
+++ McAfeeEPO-UserToPC.py
@@ -0,0 +1,47 @@
+import argparse
+import mcafee
+import datetime
+import getpass
+
+def parseargs():
+    parser = argparse.ArgumentParser(description= 'Consulta en el API de EPO una lista de usuarios y devuelve la lista de workstations en las que estos se loguean. By: Richard Arias Genao ;)', usage='%(prog)s [-h] [-i] [-o]')
+    parser.add_argument('-i', help='-i <input file (user file)>')
+    parser.add_argument('-o', help='-o <output file (workstations file)>, if not exist, create a new one')
+    #parser.add_argument('-u', help='-u <EPO Username>')
+    #parser.add_argument('-p', help='-p <EPO Password>')
+    parseargs.args = parser.parse_args()
+
+
+
+def EpoAuth():
+    print('UserToPC, by Richard Arias Genao ;)')
+    username = raw_input("Enter your EPO user: ")
+    password = getpass.getpass(prompt='Password:', stream=None)
+
+    #Authentication with McAfee EPO Api
+    EpoAuth.mc = mcafee.client("co03vepo","8443",username,password,output='json')
+
+
+def lookup():
+
+    inputfile = open (parseargs.args.i,'r')
+    outputfile = open (parseargs.args.o,'w')
+
+    for line in inputfile:
+        systems = EpoAuth.mc.system.find(line.rstrip('\n'))
+        for system in systems:
+            hostname = system['EPOComputerProperties.IPHostName']
+            epotag = system['EPOLeafNode.Tags']
+
+            if epotag != "Server":  # <-- If dont print servers
+                print >> outputfile, hostname
+
+    outputfile.close()
+    inputfile.close()
+    print('View your results at specified workstation list: '+parseargs.args.o)
+
+
+parseargs()
+EpoAuth()
+lookup()
+
